88061203c52b9db6f42304a2058c19d99f49004c
Vagnaros <vagnaros.viking@gmail.com>
2023-12-01 17:01:20 -0700

    Initial Commit

---
+++ TheWorld-Server/ServerWindow.cpp
@@ -0,0 +1,43 @@
+#include "ServerWindow.h" // - Local Preprocessor
+#include <SDL.h>		  // - Global Preprocessor
+
+int main(int argc, char* args[]) {
+
+    //Apply the image stretched
+    SDL_Rect stretchRect;
+    stretchRect.x = 0;
+    stretchRect.y = 0;
+    stretchRect.w = scrWidth;
+    stretchRect.h = scrHeight;
+    
+
+    //Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        //Load media
+        if (!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            //Apply the image
+            SDL_BlitScaled(gBackground, NULL, gScreenSurface, &stretchRect);
+            
+            //Update the surface
+            SDL_UpdateWindowSurface(gWindow);
+            
+            //Use Event loop to keep window open until window close event is manualy triggered by user
+            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+	return 0;
+}
---
+++ TheWorld-Server/ServerWindow.h
@@ -0,0 +1,100 @@
+//Declare Preprocessors
+#include <SDL.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int scrWidth = 800;
+const int scrHeight = 600;
+
+//Create SDL Pointers/Containers
+SDL_Window*  gWindow = NULL;		//The window we'll be rendering to
+SDL_Surface* gScreenSurface = NULL; //The surface contained by the window
+SDL_Surface* gBackground = NULL;        	//The image we will load and show on the screen
+SDL_Surface* loadSurface(std::string path) //Loads Individual Image
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+    if (loadedSurface == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    } else {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+        if (optimizedSurface == NULL)
+        {
+            printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return optimizedSurface;
+}
+
+//Declare Function Prototypes
+bool init();		//Starts up SDL and creates window
+bool loadMedia();	//Loads media
+void close();		//Frees media and shuts down SDL
+
+//Define Functions
+bool init() {
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow("TheWorld - Server",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            scrWidth, scrHeight, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+bool loadMedia() {
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gBackground = loadSurface("media/background.bmp");
+    if (gBackground == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/background.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+void close() {
+    //Deallocate surface
+    SDL_FreeSurface(gBackground);
+    gBackground = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
