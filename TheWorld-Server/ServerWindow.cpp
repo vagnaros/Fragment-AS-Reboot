f713135992f942c1d46e71fca0dd7610a4ccda43
Vagnaros <vagnaros.viking@gmail.com>
2023-12-04 11:50:52 -0700

    Font and Opt + Began Button Logic

--- TheWorld-Server/ServerWindow.cpp
+++ TheWorld-Server/ServerWindow.cpp
@@ -1,5 +1,4 @@
-#include "ServerWindow.h" // - Local Preprocessor
-#include <SDL.h>		  // - Global Preprocessor
+#include "ServerWindow.h" // - Local Preprocessor (Using Interal Global Preprocessors)
 
 int main(int argc, char* args[]) {
 
@@ -25,20 +24,48 @@ int main(int argc, char* args[]) {
         }
         else
         {
-            //Apply the image
-            SDL_BlitScaled(gBackground, NULL, gScreenSurface, &stretchRect);
 
-            //Apply GUI Surfaces to Screen Surface as Rects
-            SDL_BlitScaled(gGUISurfaces[GUI_SURFACE_SERVER], NULL, gScreenSurface, &serverBttn);
-            SDL_BlitScaled(gGUISurfaces[GUI_SURFACE_MAPEDIT], NULL, gScreenSurface, &mapEditBttn);
-            SDL_BlitScaled(gGUISurfaces[GUI_SURFACE_SETTINGS], NULL, gScreenSurface, &settingsBttn);
-            SDL_BlitScaled(gGUISurfaces[GUI_SURFACE_EXIT], NULL, gScreenSurface, &exitBttn);
-            
-            //Update the surface
-            SDL_UpdateWindowSurface(gWindow);
-            
+            //Current animation frame
+            int frame = 0;
+
             //Use Event loop to keep window open until window close event is manualy triggered by user
-            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+            SDL_Event e; bool quit = false;  while (quit == false) { while (SDL_PollEvent(&e) != 0) { if (e.type == SDL_QUIT) quit = true; } 
+                //Clear screen
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(gRenderer);
+
+                //Render background texture to screen
+                gBackgroundTexture.render(0, 0);
+
+                //Render current frame
+                SDL_Rect* currentClip = &gSpriteClips[frame / 4]; //Speed can be controlled [frame / 96] etc
+
+                //Render GUI Sprites to the screen
+                gGUITextures[GUI_SS_THEWORLD].render(0, 0, currentClip);
+                gTextTextures[GUI_SS_THEWORLD].render(20, 80); //TTF Text
+                gGUITextures[GUI_SS_MAILER].render(0, 100, currentClip);
+                gTextTextures[GUI_SS_MAILER].render(0, 180); //TTF Text
+                gGUITextures[GUI_SS_NEWS].render(0, 200, currentClip);
+                gTextTextures[GUI_SS_NEWS].render(20, 280); //TTF Text
+                gGUITextures[GUI_SS_ACCESSORY].render(0, 300, currentClip);
+                gTextTextures[GUI_SS_ACCESSORY].render(20, 380); //TTF Text
+                gGUITextures[GUI_SS_AUDIO].render(0, 400, currentClip);
+                gTextTextures[GUI_SS_AUDIO].render(20, 480); //TTF Text
+                gGUITextures[GUI_SS_DATA].render(0, 500, currentClip);
+                gTextTextures[GUI_SS_DATA].render(10, 580); //TTF Text
+
+                //Update screen
+                SDL_RenderPresent(gRenderer);
+
+                //Go to next frame
+                ++frame;
+
+                //Cycle animation
+                if (frame / 4 >= GUI_ANIMATION_FRAMES)
+                {
+                    frame = 0;
+                }
+            }
         }
     }
 
--- TheWorld-Server/ServerWindow.h
+++ TheWorld-Server/ServerWindow.h
@@ -1,22 +1,14 @@
-//Declare Preprocessors
+//Declare Global Preprocessors (Means the Carries over when this doc is used as local preproccessor)
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <string>
+#include "ClassWrapper.h"
 
 //Screen dimension constants
 const int scrWidth = 800;
 const int scrHeight = 600;
-
-//GUI Surfaces Constants
-enum GUISurfaces
-{
-    GUI_SURFACE_SERVER,
-    GUI_SURFACE_MAPEDIT,
-    GUI_SURFACE_SETTINGS,
-    GUI_SURFACE_EXIT,
-    GUI_SURFACE_TOTAL
-};
 
 //Create SDL Rectangle Containers for GUI Elements
 SDL_Rect serverBttn;
@@ -25,14 +17,130 @@ SDL_Rect settingsBttn;
 SDL_Rect exitBttn;
 
 //Create SDL Pointers/Containers
-SDL_Window*  gWindow = NULL;		//The window we'll be rendering to
-SDL_Surface* gScreenSurface = NULL; //The surface contained by the window
-SDL_Surface* gBackground = NULL;        	    //The image we will load and show on the screen
-SDL_Surface* gGUISurfaces[GUI_SURFACE_TOTAL];   //The images that correspond to a keypress
-SDL_Surface* loadSurface(std::string path)      //Loads Individual Images
+SDL_Window* gWindow = NULL;		//The window we'll be rendering to
+SDL_Renderer* gRenderer = NULL;     //The Renderer We'll Be Using
+
+//Declare Function Prototypes
+bool init();		//Starts up SDL and creates window
+bool loadMedia();	//Loads media
+void close();		//Frees media and shuts down SDL
+
+LTexture::LTexture()
 {
-    //The final optimized image
-    SDL_Surface* optimizedSurface = NULL;
+    //Initialize
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+LTexture::~LTexture()
+{
+    //Deallocate
+    free();
+}
+
+LButton::LButton()
+{
+    mPosition.x = 0;
+    mPosition.y = 0;
+
+    mCurrentState = BUTTON_MOUSE_OUT;
+}
+
+//Button constants
+const int BUTTON_WIDTH = 96;
+const int BUTTON_HEIGHT = 96;
+const int TOTAL_BUTTONS = 7;
+
+//Globally used font
+TTF_Font* gFont = NULL;
+
+//Rendered texture
+LTexture gTextTextures[GUI_SS_TOTAL];
+LTexture gGUIButtonStatesTexture;
+
+//Scene textures
+LTexture gGUITextures[GUI_SS_TOTAL];
+LTexture gBackgroundTexture;
+
+//GUI animation
+const int GUI_ANIMATION_FRAMES = 96;
+SDL_Rect gSpriteClips[GUI_ANIMATION_FRAMES];
+SDL_Rect gStateClips[BUTTON_TOTAL];
+
+void LButton::setPosition(int x, int y)
+{
+    mPosition.x = x;
+    mPosition.y = y;
+}
+void LButton::handleEvent(SDL_Event* e)
+{
+    //If mouse event happened
+    if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+    {
+        //Get mouse position
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+        
+        //Check if mouse is in button
+        bool inside = true;
+
+        //Mouse Location Logic (inside or outside?)
+        if (x < mPosition.x)
+        {
+            inside = false;
+        }
+        else if (x > mPosition.x + BUTTON_WIDTH)
+        {
+            inside = false;
+        }
+        else if (y < mPosition.y)
+        {
+            inside = false;
+        }
+        else if (y > mPosition.y + BUTTON_HEIGHT)
+        {
+            inside = false;
+        }
+
+        //Mouse is outside button
+        if (!inside)
+        {
+            mCurrentState = BUTTON_MOUSE_OUT;
+        }
+        //Mouse is inside button
+        else
+        {
+            //Set mouse over sprite
+            switch (e->type)
+            {
+            case SDL_MOUSEMOTION:
+                mCurrentState = BUTTON_MOUSE_OVER_MOTION;
+                break;
+
+            case SDL_MOUSEBUTTONDOWN:
+                mCurrentState = BUTTON_MOUSE_DOWN;
+                break;
+
+            case SDL_MOUSEBUTTONUP:
+                mCurrentState = BUTTON_MOUSE_UP;
+                break;
+            }
+        }
+    }
+}
+void LButton::render()
+{
+    //Show current button sprite
+    gGUIButtonStatesTexture.render(mPosition.x, mPosition.y, &gStateClips[mCurrentState]);
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+    //Get rid of preexisting texture
+    free();
+
+    //The final texture
+    SDL_Texture* newTexture = NULL;
 
     //Load image at specified path
     SDL_Surface* loadedSurface = IMG_Load(path.c_str());
@@ -42,24 +150,101 @@ SDL_Surface* loadSurface(std::string path)      //Loads Individual Images
     }
     else
     {
-        //Convert surface to screen format
-        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
-        if (optimizedSurface == NULL)
+        //Color key image
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 181, 230, 29));
+
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        if (newTexture == NULL)
         {
-            printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+            printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        }
+        else
+        {
+            //Get image dimensions
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
         }
 
         //Get rid of old loaded surface
         SDL_FreeSurface(loadedSurface);
     }
 
-    return optimizedSurface;
+    //Return success
+    mTexture = newTexture;
+    return mTexture != NULL;
 }
 
-//Declare Function Prototypes
-bool init();		//Starts up SDL and creates window
-bool loadMedia();	//Loads media
-void close();		//Frees media and shuts down SDL
+#if defined(SDL_TTF_MAJOR_VERSION)
+bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+{
+    //Get rid of preexisting texture
+    free();
+
+    //Render text surface
+    SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
+    if (textSurface == NULL)
+    {
+        printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+    }
+    else
+    {
+        //Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+        if (mTexture == NULL)
+        {
+            printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //Get image dimensions
+            mWidth = textSurface->w;
+            mHeight = textSurface->h;
+        }
+
+        //Get rid of old surface
+        SDL_FreeSurface(textSurface);
+    }
+
+    //Return success
+    return mTexture != NULL;
+}
+#endif
+
+void LTexture::free()
+{
+    //Free texture if it exists
+    if (mTexture != NULL)
+    {
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+void LTexture::render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE)
+{
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+    //Set clip rendering dimensions
+    if (clip != NULL)
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+
+    //Render to screen
+    SDL_RenderCopy(gRenderer, mTexture, clip, &renderQuad);
+}
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+int LTexture::getHeight()
+{
+    return mHeight;
+}
 
 //Define Functions
 bool init() {
@@ -67,48 +252,57 @@ bool init() {
     bool success = true;
 
     //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         success = false;
-    }
-    else
-    {
+    } else {
+
         //Create window
         gWindow = SDL_CreateWindow("TheWorld - Server",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
             scrWidth, scrHeight, SDL_WINDOW_SHOWN);
-        if (gWindow == NULL)
-        {
+        if (gWindow == NULL){
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
             success = false;
-        }
-        else
-        {
-            //Initialize PNG loading
-            int imgFlags = IMG_INIT_PNG;
-            if (!(IMG_Init(imgFlags) & imgFlags))
-            {
-                printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        } else {
+
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (gRenderer == NULL){
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
                 success = false;
-            }
-            else
-            {
-                //Get window surface
-                gScreenSurface = SDL_GetWindowSurface(gWindow);
+            } else {
 
-                //Create Position and Scale GUI Elements Rects
-                serverBttn.x = 0; serverBttn.y = 0;
-                serverBttn.w = 80; serverBttn.h = 80;
+                //Initialize renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-                mapEditBttn.x = 0; mapEditBttn.y = 100;
-                mapEditBttn.w = 80; mapEditBttn.h = 80;
+                //Initialize WEBP loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags)){
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                } else {
 
-                settingsBttn.x = 0; settingsBttn.y = 200;
-                settingsBttn.w = 80; settingsBttn.h = 80;
+                    //Create Position and Scale GUI Elements Rects
+                    serverBttn.x = 0; serverBttn.y = 0;
+                    serverBttn.w = 80; serverBttn.h = 80;
 
-                exitBttn.x = 0; exitBttn.y = 300;
-                exitBttn.w = 80; exitBttn.h = 80;
+                    mapEditBttn.x = 0; mapEditBttn.y = 100;
+                    mapEditBttn.w = 80; mapEditBttn.h = 80;
+
+                    settingsBttn.x = 0; settingsBttn.y = 200;
+                    settingsBttn.w = 80; settingsBttn.h = 80;
+
+                    exitBttn.x = 0; exitBttn.y = 300;
+                    exitBttn.w = 80; exitBttn.h = 80;
+
+                    //Initialize SDL_ttf
+                    if (TTF_Init() == -1)
+                    {
+                        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+                        success = false;
+                    }
+                }
             }
         }
     }
@@ -120,54 +314,104 @@ bool loadMedia() {
     bool success = true;
 
     //Load splash image
-    gBackground = loadSurface("media/bgImage.webp");
-    if (gBackground == NULL)
+    if (!gBackgroundTexture.loadFromFile("media/bgImage.png"))
     {
-        printf("Unable to load image %s! SDL Error: %s\n", "media/bgImage.webp", SDL_GetError());
+        printf("Unable to load image %s! SDL Error: %s\n", "media/bgImage.png", SDL_GetError());
         success = false;
     }
 
-    gGUISurfaces[GUI_SURFACE_SERVER] = loadSurface("media/GUI/server.png");
-    if (gGUISurfaces[GUI_SURFACE_SERVER] == NULL) {
-        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/server.png", SDL_GetError());
+    if (!gGUITextures[GUI_SS_THEWORLD].loadFromFile("media/GUI/TheWorldSS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/TheWorldSS.png", SDL_GetError());
+        success = false;
+    }
+    if (!gGUITextures[GUI_SS_MAILER].loadFromFile("media/GUI/MailerSS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/MailerSS.png", SDL_GetError());
+        success = false;
+    }
+    if (!gGUITextures[GUI_SS_NEWS].loadFromFile("media/GUI/NewsSS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/NewsSS.png", SDL_GetError());
+        success = false;
+    }
+    if (!gGUITextures[GUI_SS_ACCESSORY].loadFromFile("media/GUI/AccessorySS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/AccessorySS.png", SDL_GetError());
+        success = false;
+    }
+    if (!gGUITextures[GUI_SS_AUDIO].loadFromFile("media/GUI/AudioSS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/AudioSS.png", SDL_GetError());
+        success = false;
+    }
+    if (!gGUITextures[GUI_SS_DATA].loadFromFile("media/GUI/DataSS.png")) {
+        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/DataSS.png", SDL_GetError());
         success = false;
     }
 
-    gGUISurfaces[GUI_SURFACE_MAPEDIT] = loadSurface("media/GUI/map.png");
-    if (gGUISurfaces[GUI_SURFACE_MAPEDIT] == NULL) {
-        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/map.png", SDL_GetError());
-        success = false;
+    //Iterate through the 96 array items
+    for (int i = 0; i < 96; i++) {
+        //Set sprite clips
+        gSpriteClips[i].x = 96 * i; //Increments at 96px starting with 0 (96 * 0)
+        gSpriteClips[i].y = 0;
+        gSpriteClips[i].w = 96;
+        gSpriteClips[i].h = 96;
     }
 
-    gGUISurfaces[GUI_SURFACE_SETTINGS] = loadSurface("media/GUI/settings.png");
-    if (gGUISurfaces[GUI_SURFACE_SETTINGS] == NULL) {
-        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/settings.png", SDL_GetError());
+    //Open the font
+    gFont = TTF_OpenFont("media/GUI/font.ttf", 18);
+    if (gFont == NULL)
+    {
+        printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
         success = false;
     }
-
-    gGUISurfaces[GUI_SURFACE_EXIT] = loadSurface("media/GUI/exit.png");
-    if (gGUISurfaces[GUI_SURFACE_EXIT] == NULL) {
-        printf("Unable to load image %s! SDL Error: %s\n", "media/GUI/exit.png", SDL_GetError());
-        success = false;
+    else
+    {
+        //Render text
+        SDL_Color textColor = { 3, 157, 252 };
+        if (!gTextTextures[GUI_SS_THEWORLD].loadFromRenderedText("Server", textColor))
+        {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
+        if (!gTextTextures[GUI_SS_MAILER].loadFromRenderedText("Announcer", textColor)) {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
+        if (!gTextTextures[GUI_SS_NEWS].loadFromRenderedText("Events", textColor)) {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
+        if (!gTextTextures[GUI_SS_ACCESSORY].loadFromRenderedText("Areas", textColor)) {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
+        if (!gTextTextures[GUI_SS_AUDIO].loadFromRenderedText("Admin", textColor)) {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
+        if (!gTextTextures[GUI_SS_DATA].loadFromRenderedText("MeshNet", textColor)) {
+            printf("Failed to render text texture!\n");
+            success = false;
+        }
     }
 
     return success;
 }
 void close() {
-    //Deallocate surface
-    SDL_FreeSurface(gBackground);
-    gBackground = NULL;
-    SDL_FreeSurface(gGUISurfaces[GUI_SURFACE_SERVER, GUI_SURFACE_MAPEDIT, GUI_SURFACE_SETTINGS, GUI_SURFACE_EXIT]);
-    gGUISurfaces[GUI_SURFACE_SERVER] = NULL;
-    gGUISurfaces[GUI_SURFACE_MAPEDIT] = NULL;
-    gGUISurfaces[GUI_SURFACE_SETTINGS] = NULL;
-    gGUISurfaces[GUI_SURFACE_EXIT]      = NULL;
+    //Free loaded images
+    gGUITextures[GUI_SS_THEWORLD, GUI_SS_MAILER, GUI_SS_NEWS, GUI_SS_ACCESSORY, GUI_SS_AUDIO, GUI_SS_DATA].free();
+    gBackgroundTexture.free();
+    gTextTextures[GUI_SS_THEWORLD, GUI_SS_MAILER, GUI_SS_NEWS, GUI_SS_ACCESSORY, GUI_SS_AUDIO, GUI_SS_DATA].free();
+
+    //Free global font
+    TTF_CloseFont(gFont);
+    gFont = NULL;
 
     //Destroy window
     SDL_DestroyWindow(gWindow);
+    SDL_DestroyRenderer(gRenderer);
     gWindow = NULL;
+    gRenderer = NULL;
 
     //Quit SDL subsystems
+    TTF_Quit();
     IMG_Quit();
     SDL_Quit();
 }
